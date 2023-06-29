11405476b8083274a4f52a8030773630e60fa50e
Rintaro Fujita <rinpc356@gmail.com>
2023-06-29 11:01:36 +0900

    Create index.tsx

---
+++ reactlocal/reactlocal/src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import App from './App';
+// import Hello from './components/Hello'
+import reportWebVitals from './reportWebVitals';
+// import Parent from './components/ContainerSample';
+// import Message from './components/Message';
+// import ContextSample from './components/ContextSample'
+// import Counter from './components/Counter';
+import {Parent} from './components/Parent';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Parent />
+    {/* Counter initialValue={0} */}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
